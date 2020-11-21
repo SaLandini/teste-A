de4eb1cd6fa9ca44ef4eae4e2af1cdbb4a1f79af
SaLandini <62630050+SaLandini@users.noreply.github.com>
2020-11-21 19:17:10 -0300

    adding new anjo, and making some corrections

--- scripts/js.js
+++ scripts/js.js
@@ -10,28 +10,33 @@ image.src = imag
 button.addEventListener('click', () => {
     anjo = Math.floor(Math.random() * 10);
     console.log(anjo)
-    if (anjo === 0){
-        imag = "https://static.wikia.nocookie.net/neongenesisevangelion/images/5/53/15_C280A_giant-of-light.jpg/revision/latest/scale-to-width-down/340?cb=20120321165331&path-prefix=pt-br"
+    if (anjo === 0 || anjo === 7){
+        imag = "assets/anjos/adam.png"
         image.src = imag
         sub.innerHTML = "Adam"
         pe.innerHTML = "Aperte Novamente"
-    } else if (anjo === 1 || anjo === 4 || anjo === 7){
-        imag = "https://static.zerochan.net/Lilith.%28Evangelion%29.full.2929932.png"
+    } else if (anjo === 1 || anjo === 4 ){
+        imag = "assets/anjos/lilith.png"
         image.src = imag
         sub.innerHTML = "Lilith"
         pe.innerHTML = "Aperte Novamente"
-    } else if (anjo === 2 || anjo === 5 || anjo === 8){
-        imag = "https://static.zerochan.net/Sachiel.%28Evangelion%29.full.2921624.png"
+    } else if (anjo === 2 || anjo === 5){
+        imag = "assets/anjos/Sachiel.png"
         image.src = imag
         sub.innerHTML = "Sachiel"
         pe.innerHTML = "Aperte Novamente"
-    } else if (anjo === 3 || anjo === 6|| anjo === 9){
-        imag = "https://imgsapp2.correiobraziliense.com.br/app/noticia_127983242361/2014/01/15/408060/20140115175017763159i.jpg"
+    } else if (anjo === 3 || anjo === 6){
+        imag = "assets/anjos/sandler.png"
         image.src = imag
         sub.innerHTML = "Adam Sandler"
         pe.innerHTML = "Aperte Novamente"
+    } else if (anjo === 8 || anjo === 9){
+        imag = "assets/anjos/hulk.png"
+        image.src = imag
+        sub.innerHTML = "Hulk Agiota"
+        pe.innerHTML = "Aperte Novamente"
     } else {
-        imag = "https://static.wikia.nocookie.net/neongenesisevangelion/images/1/19/Adams.jpg/revision/latest/scale-to-width-down/340?cb=20120314210415&path-prefix=pt-br"
+        imag = "assets/init.png"
         image.src = imag
         sub.innerHTML = null
         pe.innerHTML = "Aperte"
